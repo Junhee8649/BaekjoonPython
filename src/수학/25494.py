e32ca401bec90ac295162bed92d012b3724dc2ee
Junhee-lee <sk5514224@gmail.com>
2025-04-30 17:44:17 +0900

    solve 25494

---
+++ src/수학/25494.py
@@ -0,0 +1,10 @@
+t = int(input())
+for n in range(t):
+    count = 0
+    a, b, c = map(int, input().split())
+    for i in range(1, a+1):
+        for j in range(1, b+1):
+            for k in range(1, c+1):
+                if i % j == j % k == k % i:
+                    count += 1
+    print(count)

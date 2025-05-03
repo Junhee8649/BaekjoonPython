66f8729a0f3d072e250da16a1edff126c5d59659
Junhee-lee <sk5514224@gmail.com>
2025-05-03 14:44:10 +0900

    solve 11051(resolve, dp와 이항계수)

---
+++ 매우헤매거나못푼문제/11051.py
@@ -0,0 +1,12 @@
+n, k = map(int, input().split())
+mod = 10007
+
+# 동적 계획법을 이용한 이항 계수 계산
+dp = [0] * (k + 1)
+dp[0] = 1
+
+for i in range(1, n + 1):
+    for j in range(min(i, k), 0, -1):
+        dp[j] = (dp[j] + dp[j-1]) % mod
+
+print(dp[k])

ed1792bf0d15ec1ca4ee2c8b4be688bc26b2e971
Junhee-lee <sk5514224@gmail.com>
2025-07-15 10:57:04 +0900

    solve 15650

---
+++ src/15649.py
@@ -0,0 +1,19 @@
+from itertools import combinations
+
+N, M = map(int, input().split())
+num_list = [num for num in range(1,N+1)]
+
+comb = combinations(num_list, M)
+sort_list = []
+
+for num in comb:
+    sort_list.append(list(num))
+
+for sub_list in sort_list:
+    sub_list.sort()
+sort_list.sort()
+
+for answer in sort_list:
+    for i in answer:
+        print(i, end=" ")
+    print()

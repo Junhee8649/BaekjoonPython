3f427ed4cd3264b2aeb6aac2ce7c229fae823ef1
Junhee-lee <sk5514224@gmail.com>
2025-07-15 11:20:12 +0900

    solve 15649 and 15650 정정

--- src/15650.py
+++ src/15650.py
@@ -1,12 +1,17 @@
-from itertools import permutations
+from itertools import combinations
 
 N, M = map(int, input().split())
 num_list = [num for num in range(1,N+1)]
-comb = permutations(num_list, M)
+
+comb = combinations(num_list, M)
 sort_list = []
 
 for num in comb:
     sort_list.append(list(num))
+
+for sub_list in sort_list:
+    sub_list.sort()
+sort_list.sort()
 
 for answer in sort_list:
     for i in answer:

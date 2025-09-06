c60c355a4d57cbfb95bdaa95312db737090484ff
Junhee-lee <sk5514224@gmail.com>
2025-09-06 23:57:44 +0900

    solve 15722

---
+++ src/15722.py
@@ -0,0 +1,29 @@
+n = int(input())
+time = 0
+count = 1
+minus = False
+x, y = 0, 0
+
+while time < n:  # <= 대신 < 사용
+    if x == y:
+        for _ in range(count):
+            if time >= n:  # 움직이기 전에 먼저 체크
+                break
+            if minus:
+                y -= 1
+            else:
+                y += 1
+            time += 1
+    else:
+        for _ in range(count):
+            if time >= n:  # 움직이기 전에 먼저 체크
+                break
+            if minus:
+                x -= 1
+            else:
+                x += 1
+            time += 1
+        count += 1
+        minus = not minus
+
+print(x, y)

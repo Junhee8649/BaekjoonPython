cb086d622aad34d5cb2429d789a30f8ac2f02abc
Junhee-lee <sk5514224@gmail.com>
2025-10-15 16:57:19 +0900

    solve 2579

---
+++ src/2579.py
@@ -0,0 +1,17 @@
+stair_count = int(input())
+stairs  = [0] * 301  # 계단 계수가 300개 이하
+for i in range(1, stair_count + 1):
+    stairs[i] = int(input())
+
+dp = [0] * 301
+
+if stair_count == 1:
+    print(stairs[1])
+else:
+    dp[1] = stairs[1]
+    dp[2] = stairs[1] + stairs[2]
+
+    for i in range(3, stair_count + 1):
+        dp[i] = max(dp[i-2] + stairs[i], dp[i-3] + stairs[i-1] + stairs[i])
+
+    print(dp[stair_count])

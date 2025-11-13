22eb29a850874159fcfbfa587747ea6275ddc8e8
Junhee-lee <sk5514224@gmail.com>
2025-11-13 22:55:05 +0900

    solve 23812

---
+++ src/23812.py
@@ -0,0 +1,18 @@
+N = int(input())
+
+for i in range(N):
+    print("@"*N, end="")
+    print(" "*N*3, end="")
+    print("@"*N)
+for i in range(N):
+    print("@"*N*5)
+for i in range(N):
+    print("@"*N, end="")
+    print(" "*N*3, end="")
+    print("@"*N)
+for i in range(N):
+    print("@"*N*5)
+for i in range(N):
+    print("@"*N, end="")
+    print(" "*N*3, end="")
+    print("@"*N)

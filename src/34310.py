6e649a476f6390ea94830e4756b7a5e7ac0ffdaa
Junhee-lee <sk5514224@gmail.com>
2026-01-05 17:45:25 +0900

    solve 34310

---
+++ src/34310.py
@@ -0,0 +1,13 @@
+N = int(input())
+people = 0
+for _ in range(N):
+    type = input().split()
+    type_count = int(type[2])
+    if type[1] == "IN":
+        people += type_count
+    else:
+        people -= type_count
+if people == 0:
+    print("NO STRAGGLERS")
+else:
+    print(people)

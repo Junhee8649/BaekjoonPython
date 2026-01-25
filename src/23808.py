c39d582b28feb318b17d31710000aab825c46d4e
Junhee-lee <sk5514224@gmail.com>
2026-01-25 22:25:24 +0900

    solve 23808

---
+++ src/23808.py
@@ -0,0 +1,10 @@
+N = int(input())
+for _ in range(N):
+    print("@"*N + "   "*N + "@"*N)
+    print("@"*N + "   "*N + "@"*N)
+for _ in range(N):
+    print("@@@@@"*N)
+for _ in range(N):
+    print("@"*N + "   "*N + "@"*N)
+for _ in range(N):
+    print("@@@@@"*N)
